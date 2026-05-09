eed1192fb537a64cc7f87f2250100fb3e6c2575a
SneatX <santi02005@hotmail.com>
2026-05-09 17:25:02 -0500

    feat: :sparkles: Update routing and type exports for new entities

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import LocationsPage from '@/pages/admin/LocationPage'
+import RestaurantTablePage from '@/pages/admin/RestaurantTablePage'
+import ReservationPage from '@/pages/admin/ReservationPage'
+import SchedulePage from '@/pages/admin/SchedulePage'
 
 export default function AppRouter() {
   return (
@@ -7,6 +10,9 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Navigate to="/admin/locations" replace />} />
         <Route path="/admin/locations" element={<LocationsPage />} />
+        <Route path="/admin/tables" element={<RestaurantTablePage />} />
+        <Route path="/admin/reservations" element={<ReservationPage />} />
+        <Route path="/admin/schedules" element={<SchedulePage />} />
       </Routes>
     </BrowserRouter>
   )
